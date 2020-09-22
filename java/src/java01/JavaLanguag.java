4e2f741751dc7045b97de4052960f38d99138713
吴政文 <71577812+wuzhenweng@users.noreply.github.com>
2020-09-22 20:21:16 +0800

    java语言概述

---
+++ java/src/java01/JavaLanguag.java
@@ -0,0 +1,10 @@
+package java01;/**
+ * @author shkstart
+ * @create 2020-09-22 20:01
+ */
+
+public class JavaLanguag {
+	public  static  void main(String [] args){
+		System.out.print("jiayou");
+	}
+}
