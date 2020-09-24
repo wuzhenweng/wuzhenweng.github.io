b55a1197bf02b52aa6f3d66c9ab459ea69891f48
吴政文 <2405181714@qq.com>
2020-09-24 17:18:07 +0800

    java编码风格

---
+++ java/src/java01/Style.java
@@ -0,0 +1,46 @@
+package java01;
+
+/**
+ * @author wzw
+ * @create 2020-09-24 16:39
+ */
+public class Style {
+	public static void main(String[] args) {
+     //编码风格
+
+		/*
+		1.使用文档注释来注释整个类或整个方法
+		如果注释的方法中的某一个步骤用单行注释或者多行注释
+
+		2.正确的缩进使用一次tab操作，进行缩进
+		  运算符两边习惯上各加一个空格比如 2 + 4.
+
+        3.Java 编码风格
+        行尾风
+        public class Test{
+        public static void main(String[] args) {
+        }
+       }
+       此行风
+       public class Test
+       {
+        public static void main(String[] args)
+         {
+         }
+       }
+
+       常用的文本编辑：
+       记事本
+       UltraEdit
+       EditPlus
+       TextPad
+       NotePad
+
+       Java集成开发环境（IDE):
+       Eclipse
+       IntelliJ IDEA :JetBrains公司
+		* */
+
+
+	}
+}
