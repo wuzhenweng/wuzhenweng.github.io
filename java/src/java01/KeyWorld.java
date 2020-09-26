01eade8f2783a81770a4b0696c73365c23d0b104
吴政文 <2405181714@qq.com>
2020-09-26 19:51:13 +0800

    关键字

---
+++ java/src/java01/KeyWorld.java
@@ -0,0 +1,44 @@
+package java.src.java01;
+
+/**
+ * @author wzw
+ * @create 2020-09-26 18:05
+ * @email 2405181714@qq.com
+ */
+public class KeyWorld {
+	public static  void main(String [] args){
+		//关键字
+
+		/*
+		1.关键字就是赋予了特殊含义的字符串，关键字都是小写
+
+		比如常用的：
+		数据类型 int float double short byte long
+		char boolean void interface class enum
+
+		流程关键字：
+		if else do while switch case break continue
+		return foreach
+
+		访问权限
+		private protected public
+
+		定义类 函数
+		abstract 抽象类 	synchronized 线程
+		extends 继承 implements 接口继承
+
+		try catch finally  throws throw异常处理
+
+		pacakage import 包
+		版本不用；以后可能会用
+		比如： goto const
+		字面值：不可以改变的值
+		true false null
+
+		2.保留字
+		Java现在
+
+		* */
+
+	}
+}
