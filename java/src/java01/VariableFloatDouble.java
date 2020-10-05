b84a147cf171467faa1323caa5dde72323910cee
吴政文 <71577812+wuzhenweng@users.noreply.github.com>
2020-10-05 19:01:49 +0800

    变量分类

---
+++ java/src/java01/VariableFloatDouble.java
@@ -0,0 +1,23 @@
+package java.src.java01;
+
+/**
+ * @author wzw
+ * @create 2020-10-05 18:40
+ * @email 2405181714@qq.com
+ */
+public class VariableFloatDouble {
+	//浮点型float double
+
+	/*
+	java浮点型也有表示范围，不受操作系统的影响
+
+	表示形式：3.4，4.3f .432必须有小数部分
+
+	科学计数法5.12e2
+
+	float单精度，尾数表示范围七位有效数字。
+	double双精度，尾数表示范围14为有效数字。通常浮点数用double
+
+	java的浮点常量默认是double,声明float 声明变量时，后面加f或"F"
+	* */
+}
---
+++ java/src/java01/VariableTest.java
@@ -0,0 +1,37 @@
+package java.src.java01;
+
+/**
+ * @author wzw
+ * @create 2020-10-05 18:01
+ * @email 2405181714@qq.com
+ */
+public class VariableTest {
+	public static void main(String [] args){
+		//整数数据类型测试
+		//byte一字节 short两字节 int四字节 long八字节
+
+		//java个数据类型有固定的表数范围不会受到操作系统的影响
+
+		//Java的整型常量默认是int类型，声明Long类型需要后面加“L”或“l"
+		//bit:计算机中的最小存储单位，byte=8bit是基本存储单元
+
+		//测试基本数据类型转换
+
+		//1.
+		/*
+		自动类型转换：从小容量转到大容量
+		不会出现异常，
+		转换顺序：(byte short char)-int -long-float-double
+		* */
+
+		//2.
+		/*
+		byte short char 之间不会发生转换，他们在计算的时候会转换成int
+
+
+		* */
+
+
+
+	}
+}
