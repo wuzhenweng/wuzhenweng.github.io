14467d6b6da74d454e4eecc2145d15cb7857ba58
吴政文 <2405181714@qq.com>
2020-10-10 22:26:30 +0800

    强制类型转换

---
+++ wzwjava/src/TypeExchange.java
@@ -0,0 +1,20 @@
+/**
+ * @author wzw
+ * @create 2020-10-10 22:17
+ * @email 2405181714@qq.com
+ */
+public class TypeExchange {
+	public static  void main(String [] args){
+
+	//强制类型转换：将容量大的数据类型转换成小的数据类型
+	/*
+	使用时需要加强制转换符，比如 short S=(int)5;
+	但可能造成精度降低或者溢出，比如float 转换成int
+
+	字符串不能直接转换成基本数据类型，需要用包装类，先转换到包装类，用Integer.parseInt解析到int
+	在用自动装箱拆箱转换成基本数据类型。比如：String a="43";int i=Interger.parseInt(a);
+
+	boolean数据类型不能转换成别的数据类型
+	* */
+ }
+}
