b7fcf77eea974a3d6373a1a85f132f77e91e0bf7
吴政文 <2405181714@qq.com>
2020-10-10 22:13:58 +0800

    字符串

---
+++ wzwjava/src/StringTest.java
@@ -0,0 +1,17 @@
+/**
+ * @author wzw
+ * @create 2020-10-10 22:07
+ * @email 2405181714@qq.com
+ */
+public class StringTest {
+	public static  void main(String [] args){
+		int no=10;
+		String str="asdfghjkl";
+		String str1="qzwxecrvtbumni,l.";
+		str1=str1+str;
+		boolean b=false;
+		System.out.println(str1);
+		str1=str1+b;
+		System.out.print(str1);
+	}
+}
